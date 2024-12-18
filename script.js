227fecf86ac325db1aad8faa9e55f1634335e725
NatkimSreng <natkimsreng@gmail.com>
2024-12-19 04:49:36 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Function that runs when the button is clicked
+function celebrate() {
+  // Show the celebration message
+  const celebrationMessage = document.getElementById("celebration");
+  celebrationMessage.style.display = "block";
+}
